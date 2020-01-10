90b4ed147c3006e9f06c8fc4b9c142bd1c066d26
James Duarte <GarnetSunset@users.noreply.github.com>
2020-01-09 23:36:58 -0500

    Update speenTool.py

--- speenTool.py
+++ speenTool.py
@@ -5,15 +5,22 @@ from importlib import util
 spam_loader = util.find_spec('playsound')
 if (spam_loader is None):
     os.system("python -m pip install playsound")
-spam_loader = util.find_spec('win32con')
-if (spam_loader is None):
-    os.system("python -m pip install pypiwin32")
-    print("Run this program again if it errors out with \"win32com\" it's just a flaw of python")
-spam_loader = util.find_spec('winshell')
-if (spam_loader is None):
-    os.system("python -m pip install winshell")
-
-import playsound, random, time, winshell
+    
+import datetime, playsound, random, sys, time
 
 cwd = os.getcwd()
-playsound.playsound(cwd + "\speens\\" + random.choice(os.listdir(cwd+"\speens\\")))
+now = datetime.datetime.now()
+hour = now.strftime("%I")
+
+if len(sys.argv) == 1:
+    playsound.playsound(cwd + "\speens\\" + random.choice(os.listdir(cwd+"\speens\\")))
+    exit()
+
+if sys.argv[1] == "g":
+    if int(hour) > 12:
+        hour = hour - 12
+    currentHour = int(hour)
+    goUp = 0
+    while goUp != currentHour:
+        goUp += 1
+        playsound.playsound(cwd + "\speens\\" + random.choice(os.listdir(cwd+"\speens\\")))
