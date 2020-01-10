2543e68481a83a72f9d7905acddd561a6f6c5760
GarnetSunset <GarnetSunset@users.noreply.github.com>
2020-01-09 23:08:06 -0500

    added speens

--- speenTool.py
+++ speenTool.py
@@ -8,6 +8,7 @@ if (spam_loader is None):
 spam_loader = util.find_spec('win32con')
 if (spam_loader is None):
     os.system("python -m pip install pypiwin32")
+    print("Run this program again if it errors out with \"win32com\" it's just a flaw of python")
 spam_loader = util.find_spec('winshell')
 if (spam_loader is None):
     os.system("python -m pip install winshell")
